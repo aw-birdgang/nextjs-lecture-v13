ca1a56f9d7ebe2b9f08b4d3334275f6c8efdcb3d
birdgang <birdgang82@gmail.com>
2023-09-18 11:53:03 +0900

    add dynamic params

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -10,8 +10,7 @@ export const metadata: Metadata = {
 
 export default async function ReviewsPage() {
     const reviews = await getReviews(6);
-    console.log('[ReviewsPage] reviews:', reviews);
-
+    console.log('[ReviewsPage] rendering:', reviews.map((review) => review.slug).join(', '));
     return (
         <>
             <Heading>Reviews</Heading>
